26c091e82d4ca18d2003a96d1742b3f3c8ab261f
Rajat Bisht <rajatbisht.rb.789@gmail.com>
2022-05-21 13:12:06 +0530

    Basic Spring project with Spring Data JPA dependency

---
+++ src/main/java/com/SpringDataJpa/StudentsRecords/StudentsRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.SpringDataJpa.StudentsRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringDataJpa/StudentsRecords/StudentsRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDataJpa.StudentsRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
